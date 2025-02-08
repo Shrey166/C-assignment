932a5601c382da3ea903ed4931608a289aaecef6
Shrey <24csu270@ncuindia.edu>
2025-02-08 21:24:12 +0530

    Add files via upload

---
+++ Q1_a.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,count=0;
+    cout<<"Enter a number:";
+    cin>>n;
+    for(i=2;i<=n;i++){
+        if(n%i==0){
+            count++;
+        }
+    }
+    if(count==1){
+        cout<<"prime";
+    }
+    else{
+        cout<<"composite";
+    }
+}
